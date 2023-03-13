c3e488f57075d06aba13015112389a57554dc0aa
P0458 <p0458@student.jamk.fi>
2023-03-13 15:00:14 +0200

    week2-task2

--- src/index.js
+++ src/index.js
@@ -1,103 +1,51 @@
-const coursesEn = [
-  'Hamburger, cream sauce and poiled potates',
-  'Goan style fish curry and whole grain rice',
-  'Vegan Chili sin carne and whole grain rice',
-  'Broccoli puree soup, side salad with two napas',
-  'Lunch baguette with BBQ-turkey filling',
-  'Cheese / Chicken / Vege / Halloum burger and french fries',
-];
-const coursesFi = [
-  'Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa',
-  'Goalaista kalacurrya ja täysjyväriisiä',
-  'vegaani Chili sin carne ja täysjyväriisi',
-  'Parsakeittoa,lisäkesalaatti kahdella napaksella',
-  'Lunch baguette with BBQ-turkey filling',
-  'Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset',
+// Menu defined
+const menu = [
+  {name: 'Lingonberry jam', price: 4.0},
+  {name: 'Mushroom and bean casserole', price: 5.5},
+  {name: 'Chili-flavoured wheat', price: 3.0},
+  {name: 'Vegetarian soup', price: 4.8},
+  {name: 'Pureed root vegetable soup with smoked cheese', price: 8.0},
 ];
 
-let lang = 'fi';
-let activeMenu = coursesFi;
-
-/**
- * Renders menu content to html page
- * @param {Array} menu - array of dishes
- */
-const renderMenu = (menu) => {
-  const menuBox = document.querySelector('.menu-box');
-  menuBox.innerHTML = '';
-  const list = document.createElement('ul');
-  for (const dish of menu) {
-    const li = document.createElement('li');
-    li.textContent = dish;
-    list.append(li);
-  }
-  menuBox.append(list);
+// Validate menu items
+const validateMealName = (name) => {
+  const nameRegex = /^[A-Z][\w\s\-/,()]{3,63}$/;
+  return nameRegex.test(name);
 };
 
-renderMenu(activeMenu);
+// Showing prices low to high
+const sortedMenu = menu.sort((a, b) => a.price - b.price);
+console.log(sortedMenu);
 
-/**
- * Sorts menu alphapetically
- * @param {Array} menu - Array of dishes
- * @param {string} order - 'asc' or 'desc'
- * @returns sorted menu array
- */
-const sortMenu = (menu, order = 'asc') => {
-  menu.sort();
-  if (order === 'desc') {
-    menu.reverse();
-  }
-  return menu;
-};
+// Showing items only less than 5 €
 
-/**
- * Change UI language
- * @param {string} language
- */
-const changeLanguage = (language) => {
-  if (language === lang) {
-    // if the language is the same as the current language, toggle to the other language
-    activeMenu = language === 'fi' ? coursesEn : coursesFi;
-    lang = language === 'fi' ? 'en' : 'fi';
-  } else {
-    // if the language is different from the current language, switch to the new language
-    activeMenu = language === 'fi' ? coursesFi : coursesEn;
-    lang = language;
-  }
-  renderMenu(activeMenu);
-};
+const underFiveDishes = menu.filter((item) => item.price < 5.0);
 
-/**
- * Get a random dish fron an array
- * @param {Array} menu - Array of dishes
- * @returns random dish item
- */
-const getRandomDish = (menu) => {
-  const randomIndex = Math.floor(Math.random() * menu.length);
-  return menu[randomIndex];
-};
+// Increase all the prices by 15%
 
-const sortButton = document.querySelector('#sort-button');
-sortButton.addEventListener('click', () => {
-  renderMenu(sortMenu(activeMenu));
-});
+const increasePrices = menu.map((item) => ({
+  name: item.name,
+  price: item.price * 1.15,
+}));
 
-const langButton = document.querySelector('#lang-button');
-langButton.addEventListener('click', () => {
-  if (lang === 'fi') {
-    changeLanguage('en');
-  } else {
-    changeLanguage('fi');
-  }
-});
+// Total cost of menu
 
-const rendButton = document.querySelector('#rend-button');
-rendButton.addEventListener('click', () => {
-  const randomDish = getRandomDish(activeMenu);
-  const randomDishBox = document.createElement('div');
-  randomDishBox.textContent = randomDish;
-  document.body.append(randomDishBox);
-});
+const totalCost = menu.reduce((acc, item) => acc + item.price, 0);
 
-// TODO: Add a button for changing the language of the menu
-// TODO: Add a button that picks a random dish from the array and displays it
+// Testing validating
+
+console.log(validateMealName('Lingonberry jam'));
+
+// Testing dish prices under 5
+
+console.log(underFiveDishes);
+
+// Testing increasing prices
+
+console.log(increasePrices);
+
+// Testing total cost
+
+console.log(
+  `The total cost of eating the whole menu is: ${totalCost.toFixed(2)} €`
+);
